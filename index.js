9f14fddbf0800856ab2f8230a377ccbd39c80818
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-17 22:17:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // Code your solutions in this file
-const arryaName=('Lisa', 'Kaitlin', 'Jan'], "birthday");
-function writeCards(arryaNAme,eventName){
-  for(let i=0; i<=arryaNAme.length; i++){
-    console.log("Thank you,".concat(writeCards, ", for the wonderful birthday gift!"));
+const girls =["Ada", "Brendan", "Ali"];
+const eventName = 'surprice';
+
+function writeCards(girls,eventName){
+  const result=[];
+  for(let i = 0; i < girls.length; i++){
+    result.push(`Thank you, ${girls[i]}, for the wonderful ${eventName} gift!`);
   }
-  return(arryaNAme);
+  return result;
 }
+
+writeCards(girls,eventName);
+
+function countDown(num){
+  let i=num;
+  while(i>-1){
+    console.log(i);
+    i--;
+  }
+}
+countDown(10);
